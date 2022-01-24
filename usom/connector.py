38c8dcaf187d5a9ec3560732971029307343d633
Şemseddin AKSOY <semsaksoy@gmail.com>
2022-01-24 23:13:35 +0300

    function fixes

--- usom/connector.py
+++ usom/connector.py
@@ -1,24 +1,17 @@
 from connectors.core.connector import Connector, ConnectorError, get_logger
-
-from .operations import *
-from .operations import _check_health
-##sudo -u nginx /opt/cyops-integrations/.env/bin/pip install -r /opt/cyops-integrations/integrations/connectors/usom_1_0_0/requirements.txt
+from .operations import lookup, _check_health
 
 logger = get_logger('usom')
 
 
 class Usom(Connector):
     def execute(self, config, operation_name, params, **kwargs):
-        # raise Exception(operation_name)
-        try:
-            logger.info("operation_name: {0}".format(operation_name))
-            result = None
-            if operation_name == 'lookup_url':
-                result = query(config, params)
-            return result
-        except Exception as e:
-            logger.exception("An exception occurred {0}".format(e))
-            raise ConnectorError("{0}".format(e))
+        logger.info("operation_name: {0}".format(operation_name))
+        operations = {
+            'lookup': lookup
+        }
+        action = operations.get(operation_name)
+        return action(config, params)
 
     def check_health(self, config):
         try:
