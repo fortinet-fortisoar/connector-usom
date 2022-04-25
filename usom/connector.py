f30ac0c79a421e58bafe46db441ebc79fc9b33f5
Renuka Andhale <randhale@fortinet.com>
2022-04-25 17:49:31 +0530

    Adding feed data ingestion changes

--- usom/connector.py
+++ usom/connector.py
@@ -1,5 +1,12 @@
+""" Copyright start
+  Copyright (C) 2008 - 2022 Fortinet Inc.
+  All rights reserved.
+  FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
+  Copyright end """
+
+
 from connectors.core.connector import Connector, ConnectorError, get_logger
-from .operations import get_feed, operations, _check_health
+from .operations import fetch_indicators, operations, _check_health
 
 logger = get_logger('usom')
 
--- usom/operations.py
+++ usom/operations.py
@@ -1,27 +1,45 @@
+""" Copyright start
+  Copyright (C) 2008 - 2022 Fortinet Inc.
+  All rights reserved.
+  FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
+  Copyright end """
+
+
 from connectors.core.connector import Connector, ConnectorError, get_logger
 import requests
 from urllib.parse import urlparse
-import xmltodict
+import xmltodict, datetime, time
 
 logger = get_logger('usom')
 
 
-def get_feed(config, params):
+
+def make_rest_call(config, method='GET'):
     try:
-        url = config.get("url")
-        verify = config.get("verify")
-        response = requests.get(url, verify=verify)
-        content = xmltodict.parse(response.text)
-        content = content["usom-data"]["url-list"]
-        return content
-    except Exception as e:
-        logger.exception("An exception occurred {0}".format(e))
-        raise ConnectorError("{0}".format(e))
+        server_url = config.get('server_url')
+        response = requests.request(method, server_url, verify=config.get('verify'))
+        if response.ok:
+            return response
+        else:
+            logger.error(response.text)
+            raise ConnectorError({'status_code': response.status_code, 'message': response.reason})
+    except requests.exceptions.SSLError:
+        raise ConnectorError('SSL certificate validation failed')
+    except requests.exceptions.ConnectTimeout:
+        raise ConnectorError('The request timed out while trying to connect to the server')
+    except requests.exceptions.ReadTimeout:
+        raise ConnectorError('The server did not send any data in the allotted amount of time')
+    except requests.exceptions.ConnectionError:
+        raise ConnectorError('Invalid endpoint or credentials')
+    except Exception as err:
+        logger.exception(str(err))
+        raise ConnectorError(str(err))
+
 
 
 def _check_health(config):
     try:
-        url = config.get("url")
+        url = config.get("server_url")
         verify = config.get("verify")
         domain = urlparse(url).netloc
         protocol = "https://" if verify else "http://"
@@ -29,10 +47,50 @@ def _check_health(config):
         if response.status_code != 200:
             raise ConnectorError("Unable to connect USOM")
         return True
+    except Exception as err:
+        logger.exception('{0}'.format(err))
+        raise ConnectorError("Unable to connect USOM {0}".format(err))
+
+
+
+def convert_datetime(last_pull_time):
+    last_pull_timestamp = datetime.datetime.strptime(str(last_pull_time), "%Y-%m-%dT%H:%M:%S.%fZ").timestamp()
+    return last_pull_timestamp
+
+
+def check_duplicate_records(input_list):
+    seen = set()
+    final_result = [x for x in input_list if
+                    [x['url'].replace(" ", "") not in seen, seen.add(x['url'].replace(" ", ""))][0]]
+    return final_result
+
+
+def fetch_indicators(config, params):
+    try:
+        last_pull_time = params.get('modified_after')
+        response = make_rest_call(config)
+        content = xmltodict.parse(response.text)
+        content = content["usom-data"]["url-list"]  # 2022-04-19 07:26:29.016155
+        if last_pull_time:
+            last_pull_timestamp = convert_datetime(last_pull_time)
+            url_list = []
+            for url_dict in content["url-info"]:
+                dd = url_dict["date"]
+                try:
+                    create_date_timestamp = int(datetime.datetime.strptime(url_dict["date"], "%Y-%m-%d %H:%M:%S.%f").timestamp())
+                except:
+                    create_date_timestamp = int(datetime.datetime.strptime(url_dict["date"], "%Y-%m-%d %H:%M:%S").timestamp())
+                if create_date_timestamp > last_pull_timestamp:
+                    url_list.append(url_dict)
+            return check_duplicate_records(url_list)
+        else:
+            return check_duplicate_records(content["url-info"])
     except Exception as e:
-        raise ConnectorError("Unable to connect USOM {0}".format(e))
+        logger.exception("An exception occurred {0}".format(e))
+        raise ConnectorError("{0}".format(e))
+
 
 
 operations = {
-    'get_feed': get_feed
+    'fetch_indicators': fetch_indicators
 }
